82b4216562ed18fd1c71f1c2cd99e558f317112a
Anders F. Björklund <afb@macports.org>
2009-01-27 13:43:54 +0000

    mass upgrade from Xfce Beta to RC 1
    git-svn-id: https://svn.macports.org/repository/macports/users/afb@46027 d073be05-634f-4543-b044-5fe20cf6d1d6

--- Capybara/xfce4-panel/files/patch-launcher-exec.c
+++ Capybara/xfce4-panel/files/patch-launcher-exec.c
@@ -1,16 +1,23 @@
---- ./plugins/launcher/launcher-exec.c.orig	2009-01-12 22:35:30.000000000 +0100
-+++ ./plugins/launcher/launcher-exec.c	2009-01-15 20:27:13.000000000 +0100
-@@ -49,6 +49,13 @@
- #define WAIT_ANY (-1)
+Index: plugins/launcher/launcher-exec.c
+===================================================================
+--- plugins/launcher/launcher-exec.c	(revision 29263)
++++ plugins/launcher/launcher-exec.c	(working copy)
+@@ -63,8 +63,18 @@
+ #include <libsn/sn.h>
  #endif
  
 +#ifdef __APPLE__
++/* apple doesn't have a environ symbol */
 +#include <crt_externs.h>
 +#define environ (*_NSGetEnviron())
-+#else
-+extern char **environ;
++#elif !defined(__USE_GNU)
++/* when __USE_GNU is defined environ is defined in unistd.h
++ * this to avoid a redundant redeclaration */
++extern gchar **environ;
 +#endif
-+
- #include "launcher.h"
- #include "launcher-exec.h"
  
+ 
++
+ #ifdef HAVE_LIBSTARTUP_NOTIFICATION
+ typedef struct
+ {
